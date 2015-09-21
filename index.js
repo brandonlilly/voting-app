503b830a2b05916b03e96a0ffa8fe9253446ecc8
Brandon Lilly <brandonjlilly@gmail.com>
2015-09-21 15:08:36 -0700

    socket.io server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import configureStore from './src/store';
+import startServer from './src/server';
 
 export const store = configureStore();
+
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  enties: require('./entries.json'),
+});
+
+store.dispatch({ type: 'NEXT' });
